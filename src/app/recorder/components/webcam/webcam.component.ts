5ff36f47a7e881498d472ac7201cdd08a1cee863
Stanley Nwaegwu <kingstanley.ks16@gmail.com>
2022-12-18 22:38:16 +0100

    updated webcam recording to remove screen sharing

--- src/app/recorder/components/webcam/webcam.component.ts
+++ src/app/recorder/components/webcam/webcam.component.ts
@@ -23,14 +23,14 @@ export class WebcamComponent implements OnInit {
     this.startMedia();
   }
   async startMedia() {
-    const screen = await navigator.mediaDevices.getDisplayMedia({ audio: { echoCancellation: true, noiseSuppression: true }, });
+    // const screen = await navigator.mediaDevices.getDisplayMedia({ audio: { echoCancellation: true, noiseSuppression: true }, });
     const webcam = await navigator.mediaDevices.getUserMedia({ video: { echoCancellation: true, noiseSuppression: true }, });
     const micStream = await navigator.mediaDevices.getUserMedia({ audio: { echoCancellation: true, noiseSuppression: true }, });
 
     // videoRefs
     const screenShareVideoRef: HTMLVideoElement = document.createElement('video');
     let webcamVideoRef = document.createElement('video');
-    screenShareVideoRef.srcObject = screen;
+    // screenShareVideoRef.srcObject = screen;
     webcamVideoRef.srcObject = webcam;
     webcamVideoRef.muted = true;
     webcamVideoRef.play();
@@ -48,13 +48,7 @@ export class WebcamComponent implements OnInit {
 
     this.emitStream.emit(this.stream);
 
-    screen.getTracks()[0].onended = (evt) => {
-      console.log('screen share stopped: ', evt);
-      alert('screen recording has stopped. click on stop button to preview your recording for downloads')
-      // this.emitStop.emit('stop');
-      webcamVideoRef.pause();
-      // webcamVideoRef.hidden = true;
-    }
+
 
 
   }
